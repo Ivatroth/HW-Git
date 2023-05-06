fd0e088ee72a223dbf00ff52bea5ee7245e43923
ivatroth <ivana.frontroth@gmail.com>
2023-05-06 11:45:24 -0300

    cambio de funcion en mi Rama iva

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+    let num=0;
+    if(numero%2 === 0) num = numero.toString(2)
+    else num = numero.toString(16)
+    return num
+    
     }
