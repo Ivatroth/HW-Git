1bc75bb623c9e743019c38cbb81f22f47c3a5165
ivatroth <ivana.frontroth@gmail.com>
2023-05-06 12:09:15 -0300

    agrege funcion con mi nombre

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function laCajaDePandora(numero){
     if(numero%2 === 0) num = numero.toString(2)
     else num = numero.toString(16)
     return num
-    
+
     }
+
+const Ivana = ()=>{
+    return {
+        nombre: 'Ivana',
+        edad: 45,
+        nacionalidad: 'Argentina'
+    }
+}
