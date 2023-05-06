615504f212b8721fd79567b63fd28a0ea25cee63
nelsonf2019 <nelsonfercher@gmail.com>
2023-05-06 12:17:32 -0300

    función Nombre

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
-    if(!numero %2 == 0 ){
+    let binario = "0"
+    if(!numero %2 === 0 ){
         for (let i = 0; i < numero.length; i++) {
             const element = array[i];
-            
+            if(numero %2 === 0){
+                
+            }
         }
     }
     }
+function nombre(nombre){
+    let persona={
+        nelson:nombre,
+        edad:36,
+        nacionalidad:"Argentina"
+    }
+    return persona;
+}
