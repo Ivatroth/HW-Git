0805001370ce1852b627b6ca87faa094e4d3311f
Zarratea <112415665+Zarratea@users.noreply.github.com>
2023-05-06 12:06:18 -0300

    Resuelvo la funcion

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
-    console.log(numero)
+    if(numero%2===0){
+        let binario=numero.toString(2);
+        return binario
+    }else{
+        const hexadecimal = numero.toString(16)
+        return hexadecimal
+    }
     }
