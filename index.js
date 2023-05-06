e115854052eb29ad3fecb1c3c638c95c925e4975
nelsonf2019 <nelsonfercher@gmail.com>
2023-05-06 11:58:06 -0300

    modific

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+    if(!numero %2 == 0 ){
+        for (let i = 0; i < numero.length; i++) {
+            const element = array[i];
+            
+        }
+    }
     }
